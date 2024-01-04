1866a10977dddc6df23150ad28f006ad6cfded69
ZRNOF <ZRNOF@protonmail.com>
2024-01-04 11:26:43 +0800

    chore: add comment for mountDecoration

--- src/Decoration/Decoration.js
+++ src/Decoration/Decoration.js
@@ -1,6 +1,10 @@
 import mountBorder from "./Border/Border.js"
 import mountShadow from "./Shadow/Shadow.js"
 
+/**
+ * Mount Decoration to p5
+ * @param {p5} p5 - p5
+ */
 export const mountDecoration = (p5) => {
 	mountBorder(p5)
 	mountShadow(p5)
